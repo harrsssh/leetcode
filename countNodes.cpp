d3568edee9ce74328c410eeeb8733f992748e19b
Harsh Patidar <101214597+harrsssh@users.noreply.github.com>
2022-08-09 02:05:46 +0530

    Create countNodes.cpp
    leetcode 222. Count Complete Tree Nodes

---
+++ countNodes.cpp
@@ -0,0 +1,27 @@
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        else{
+            return 1 + countNodes(root->left) + countNodes(root->right);
+        }
+    }
+};
