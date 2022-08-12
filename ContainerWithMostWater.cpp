bdff9a482d34187d6ae91a976d3e287ee33da4ac
Harsh Patidar <101214597+harrsssh@users.noreply.github.com>
2022-08-13 00:58:54 +0530

    Create ContainerWithMostWater.cpp

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,26 @@
+//You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+//Find two lines that together with the x-axis form a container, such that the container contains the most water.
+//Return the maximum amount of water a container can store.
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = 0;
+        int i=0;
+        int n = height.size();
+        int k=0;
+        int j=n-1;
+        while(i < j){
+            area = max(area, (n-k-1)*(min(height[i],height[j])));
+            k++;
+            if(height[i] < height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        
+        return area;
+    }
+};
